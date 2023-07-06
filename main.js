8b5feeb1023ccb92f00bdd7c88cec4858a4ab2f9
marioecg <hey@marioecg.com>
2023-07-06 16:08:13 -0700

    update

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ import * as THREE from 'three'
 let width = 400
 let height = 400
 
-let renderer, camera
-let renderPass, renderTargetA, renderTargetB
-let scene, cube
-
-let orthoCamera, postScene1, postMesh1, postScene2, postMesh2
+let renderer
+let renderPassTarget, renderTargetA, renderTargetB
+let camera, orthographic
+let mainScene, passScene, postScene
+let cube, passQuad, postQuad
 
 init()
 renderer.setAnimationLoop(render)
@@ -18,97 +18,75 @@ function init() {
         antialias: false,
     })
     renderer.setSize(width, height)
-    renderer.setClearColor(0x000000, 1.0)
+    renderer.setClearColor(0x000000, 1)
 
     let container = document.querySelector('#app')
     container.appendChild(renderer.domElement)
 
     // render targets
-    renderPass = new THREE.WebGLRenderTarget(width * devicePixelRatio, height * devicePixelRatio)
+    renderPassTarget = new THREE.WebGLRenderTarget(width * devicePixelRatio, height * devicePixelRatio)
     renderTargetA = new THREE.WebGLRenderTarget(width * devicePixelRatio, height * devicePixelRatio)
     renderTargetB = new THREE.WebGLRenderTarget(width * devicePixelRatio, height * devicePixelRatio)
 
-    // camera
+    // cameras
     camera = new THREE.PerspectiveCamera(45, width / height, 1, 1000)
     camera.position.z = 5
 
-    // scene
-    scene = new THREE.Scene()
+    orthographic = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
+
+    // scenes
+    mainScene = new THREE.Scene()
+    passScene = new THREE.Scene()
+    postScene = new THREE.Scene()
+
+    // Mesh
     cube = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshNormalMaterial())
+    mainScene.add(cube)
 
-    scene.add(cube)
+    // Pass 
+    let planeGeometry = new THREE.PlaneGeometry(2, 2)
 
-    // ------------------------------
-    // post processing
-    // ------------------------------
-    orthoCamera = new THREE.OrthographicCamera(-1, 1, 1, -1, 0, 1)
-    let planeGeom = new THREE.PlaneGeometry(2, 2)
+    passQuad = new THREE.Mesh(planeGeometry, new THREE.MeshBasicMaterial({
+        map: null,
+    }))
+    passQuad.material.toneMapped = false
 
-    // Quad 1
-    postScene1 = new THREE.Scene()
+    passScene.add(passQuad)
 
-    let postMat1 = new THREE.ShaderMaterial({
+    // Post 
+    postQuad = new THREE.Mesh(planeGeometry, new THREE.ShaderMaterial({
         vertexShader: /* glsl */`
         varying vec2 vUv;
-        
-        void main () {
+
+        void main() {
             vUv = uv;
             
             gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
         }`,
+
         fragmentShader: /* glsl */`
         uniform sampler2D tDiffuse;
-
-        varying vec2 vUv;
-  
-        void main () {
-            vec2 uv = vUv;
-            uv.y += 0.05;
-
-            vec4 inputColor = texture2D(tDiffuse, uv);
-            
-            gl_FragColor = inputColor;
-            gl_FragColor.r += 0.01;
-        }`,
-        uniforms: {
-            tDiffuse: { value: null },
-        },
-    })
-
-    postMesh1 = new THREE.Mesh(planeGeom, postMat1)
-    postScene1.add(postMesh1)
-
-    // Quad 2
-    postScene2 = new THREE.Scene()
-
-    let postMat2 = new THREE.ShaderMaterial({
-        vertexShader: /* glsl */`
-        varying vec2 vUv;
         
-        void main () {
-            vUv = uv;
-            
-            gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-        }`,
-        fragmentShader: /* glsl */`
-        uniform sampler2D tDiffuse;
-
         varying vec2 vUv;
-  
-        void main () {
+
+        void main() {
             vec2 uv = vUv;
 
-            vec4 inputColor = texture2D(tDiffuse, uv);
-            
-            gl_FragColor = inputColor;
+            uv.y += 0.01;
+
+            vec4 color = texture2D(tDiffuse, vUv);
+
+            gl_FragColor = color;
+
+            gl_FragColor.a += 0.01;
         }`,
+
         uniforms: {
             tDiffuse: { value: null },
-        },
-    })
+        }
+    }))
 
-    postMesh2 = new THREE.Mesh(planeGeom, postMat2)
-    postScene2.add(postMesh2)
+    postScene.add(postQuad)
 }
 
 function render() {
@@ -116,38 +94,37 @@ function render() {
     cube.rotation.y += 0.005
     cube.rotation.z += 0.005
 
-    // "Render pass" contains the main scene, we want to render it to the renderPass
-    renderer.setRenderTarget(renderPass)
-    renderer.render(scene, camera)   
+    // --------------------
+    // don't clear the contents of the canvas on each render
+    // in order to achieve the effect, draw the new frame
+    // on top of the previous one
+    renderer.autoClearColor = false
 
-    postMesh1.material.uniforms.tDiffuse.value = renderPass.texture
-    
-    renderer.setRenderTarget(null)
-    renderer.render(postScene1, orthoCamera)
+    renderer.setRenderTarget(renderPassTarget)
+    renderer.render(mainScene, camera)
 
-    // renderer.autoClearColor = false    
+    passQuad.material.map = renderPassTarget.texture
 
-    // Explicitly set renderTargetA as the framebuffer to render to
-    renderer.setRenderTarget(renderTargetA)    
+    // set renderTargetA as the framebuffer to render to
+    renderer.setRenderTarget(renderTargetA)
 
-    // Render the postScene to renderTargetA,
+    // render postScene to renderTargetA,
     // this will contain the ping-pong accumulated texture
-    renderer.render(postScene1, orthoCamera)    
-    
-    // Render the original scene containing 3D objects on top
-    renderer.render(scene, camera)    
+    renderer.render(postScene, orthographic)
 
-    // Set the device screen as the framebuffer to render to
-    renderer.setRenderTarget(null)    
+    // render on top the original scene (texture) containing the objects
+    renderer.render(passScene, orthographic)
 
-    // Pass the target texture to the postMesh2 quad 
-    // that covers the device screen
-    postMesh2.material.uniforms.tDiffuse.value = renderTargetA.texture   
-    
-    // Render postScene2 to the default framebuffer
-    renderer.render(postScene2, orthoCamera) 
-    
-    // Swap the render targets
+    // set the device screen as the framebuffer to render to
+    renderer.setRenderTarget(null)
+
+    // pass the target texture to the quad that covers the device screen
+    postQuad.material.uniforms.tDiffuse.value = renderTargetA.texture
+
+    // render the post quad to the screen
+    renderer.render(postScene, orthographic)
+
+    // swap the render targets
     let temp = renderTargetA
     renderTargetA = renderTargetB
     renderTargetB = temp
